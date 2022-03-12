d9c44ce5bce69589d1b5099f5d280503990c77a6
Andrey K <kolomiychuk_av@ukr.net>
2022-03-12 16:32:49 +0200

    Урок 12 2 задание

--- Урок 12 Практическое задание 1ч/script.js
+++ Урок 12 Практическое задание 1ч/script.js
@@ -2,3 +2,25 @@
 
 const numberOfFilm = prompt("Сколько Вы посмотрели фильмов?", "");
 console.log(numberOfFilm);
+let movie;
+let actor;
+let genere = [];
+
+const personalMovieDB = {
+
+    count: numberOfFilm,
+    movies: movie,
+    actors: actor,
+    generes: genere,
+    private: false,
+
+};
+
+console.log(personalMovieDB.count);
+console.log(personalMovieDB.movies);
+console.log(personalMovieDB.actors);
+console.log(personalMovieDB.generes);
+console.log(personalMovieDB.private);
+
+
+
