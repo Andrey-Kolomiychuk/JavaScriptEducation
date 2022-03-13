cd8600c88eafa5a0526aaaee4bc687beb37326a1
Andrey K <kolomiychuk_av@ukr.net>
2022-03-13 19:01:34 +0200

    Урок 23 Основы ООП

---
+++ Урок 23 Основы ООП, прототипно-ориентированное наследование/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const soldier = {
+    health: 400,
+    armor: 100
+};
+
+const john = Object.create(soldier);
+
+// const Jonh = {
+//     health: 100
+// };
+
+// Jonh.__proto___ = soldier; //Устарело
+// console.log(Jonh.armor);
+
+// Object.setPrototypeOf(Jonh, soldier);
+
