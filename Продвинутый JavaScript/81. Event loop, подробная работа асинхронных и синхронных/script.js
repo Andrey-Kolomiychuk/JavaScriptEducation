e5439d435de4e80c39f7d37af497b624089ad5c9
Andrey K <kolomiychuk_av@ukr.net>
2022-03-23 11:43:16 +0200

    JavaScript Finaly

---
+++ Продвинутый JavaScript/81. Event loop, подробная работа асинхронных и синхронных/script.js
@@ -0,0 +1,31 @@
+
+'use strict';
+
+setTimeout(() => {
+    console.log(1);
+}, 0);
+
+console.log(2);
+
+function count() {
+    for (let i = 0; i < 1e9; i++ ) {
+        k++;
+    }
+    alert('done');
+}
+
+count();
+
+
+
+// console.log(1);
+//  setTimeout(() => {
+//      console.log('timeout');
+//  }, 2000);
+
+
+//  setTimeout(() => {
+//      console.log('timeout');
+//  }, 4000);
+
+//  console.log(2);
